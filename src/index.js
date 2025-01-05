6d2ac28bdff83e17b0413762f6ca5dfa4d90e507
PrashantVashisth <prashantvashisth2004@gmail.com>
2025-01-05 16:11:33 +0530

    setup city repository

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {PORT} = require('./config/serverConfig');
+const { PORT } = require('./config/serverConfig');
 const setupAndStartServer = async () =>{
 
     // create the express object
