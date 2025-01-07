4cf066aa67672144cf4ad1d1da21e92a606d6956
PrashantVashisth <prashantvashisth2004@gmail.com>
2025-01-07 10:43:51 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -8,6 +8,7 @@ class cityRepository {
             const city = await City.create({ name });
             return city;    
         } catch(error) {
+            console.log("Something wrong in repository layer.");
             throw{error};
         }
     }
@@ -20,6 +21,35 @@ class cityRepository {
                 }
             });
         } catch(error) {
+            console.log("Something wrong in repository layer.");
+            throw{error};
+        }
+    }
+
+    async getCity (cityId) {
+        try {
+            const city = await City.findbyPk(cityId);
+            // const city = await City.findOne({
+            //     where : {
+            //         id : cityId
+            //     }
+            // });
+            return city;
+        } catch(error) {
+            console.log("Something wrong in repository layer.");
+            throw{error};
+        }
+    }
+
+    async updateCity (cityId , data) {
+        try {
+            const city = await City.update(data , {
+                where : {
+                    id : cityId
+                }
+            })
+        } catch(error) {
+            console.log("Something wrong in repository layer.");
             throw{error};
         }
     }
