2ad53dea0ca63487096374ca3b5097d816bb48c0
PrashantVashisth <prashantvashisth2004@gmail.com>
2025-01-21 18:48:05 +0530

    Added airplane seeders file

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
 
-// const db = require('./models/index');
-// const sequelize = require('sequelize');
-// const { City , Airport} = require('./models/index');
+const db = require('./models/index');
+const sequelize = require('sequelize');
+const { City , Airport , Airplane} = require('./models/index');
+// const airplane = require('./models/airplane');
 
 
 const setupAndStartServer = async () =>{
@@ -18,25 +19,13 @@ const setupAndStartServer = async () =>{
 
     app.use('/api' , ApiRoutes)
 
-    app.listen(PORT , () => {
+    app.listen(PORT , async () => {
         console.log(`Server started at ${PORT}`);
+        // if(process.env.SYNC_DB){
+        //     db.sequelize.sync({alter:true});
+        // }
         
     });
-    // db.sequelize.sync({alter : true});
-    // const city = await City.findOne({
-    //     where :{
-    //         id : 13
-    //     }
-    // });
-    // console.log(city);
-    // const airports = await Airport.findAll({
-    //     where : {
-    //         cityId : city.id
-    //     }
-    // });
-    // const newAirport = await Airport.create()
-    // const airports = await city.getAirports();
-    // console.log(airports);
 }
 
 setupAndStartServer();
